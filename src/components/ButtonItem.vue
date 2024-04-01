a4dcc10b599a2f76584b59017470c5ebb3478a84
keitarleen <keitarleenkalle@gmail.com>
2024-04-01 11:38:15 +0300

    Add Button component

---
+++ src/components/ButtonItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    text?: string
+    btnType?: 'primary' | 'secondary'
+  }>(),
+  {
+    btnType: 'primary'
+  }
+)
+</script>
+
+<template>
+  <button
+    class="flex gap-2 px-3 py-2 rounded text-white items-center"
+    :class="{ [btnType]: btnType }"
+  >
+    <slot />
+    <span v-if="text" class="text-inherit font-medium text-sm">{{ text }}</span>
+  </button>
+</template>
+
+<style scoped>
+.primary {
+  background-color: var(--c-btn);
+}
+
+.primary:hover {
+  background-color: var(--c-btn-hover);
+}
+
+.secondary {
+  background-color: var(--c-white);
+  border: 1px solid var(--c-border);
+  color: var(--c-text-btn);
+
+  padding: 6px 8px;
+}
+</style>
