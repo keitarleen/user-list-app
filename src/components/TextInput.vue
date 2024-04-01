66ac73243294c50fe7f263c6b2481936997322a7
keitarleen <keitarleenkalle@gmail.com>
2024-04-01 11:39:46 +0300

    Add TextInput component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import SearchIcon from '@/components/SearchIcon.vue'
+
+defineProps<{
+  placeholder?: string
+}>()
+</script>
+
+<template>
+  <div class="grow relative">
+    <input :placeholder="placeholder" class="p-2 pl-9 rounded w-full text-sm" />
+    <span class="absolute top-2.5 left-3">
+      <SearchIcon :width="16" :height="16" />
+    </span>
+  </div>
+</template>
+
+<style scoped>
+input {
+  border: 1px solid var(--c-border);
+}
+</style>
